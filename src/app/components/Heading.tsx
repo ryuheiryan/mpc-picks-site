5bd7e3fb6406257000cfc2946c3b25868c7e90df
ryuheiryan <ryanpabz16@gmail.com>
2024-08-20 09:13:44 +0800

    Decrease heading font size for small devices

--- src/app/components/Heading.tsx
+++ src/app/components/Heading.tsx
@@ -1,6 +1,6 @@
 export default function Heading({ children }: { children: React.ReactNode }) {
   return (
-    <h2 className="font-serif text-9xl xl:text-[10rem] leading-none font-medium text-center mb-4">
+    <h2 className="font-serif text-8xl md:text-9xl xl:text-[10rem] leading-none font-medium text-center mb-4">
       {children}
     </h2>
   );
